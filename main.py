ab98b640e245c2baa245e41a89fe014f6c6060e5
hudsonft <hudsonthomas@Hudsons-MBP.student.wheaton.edu>
2018-02-08 14:33:54 -0600

    Finalized algorithm logic and added games to team path map
    The code runs now and almost completes the bracket but errors out at the end because a there are a few team names that are in the path map that aren't in the five-thirty-eight data that I have loaded. Not sure why yet, hopefully we just missed an index or something.

--- main.py
+++ main.py
@@ -14,27 +14,49 @@ path_data = BracketMap()
 # load the data and take only the columns we need.
 all_forecast_data = pd.read_csv('./fivethirtyeight_ncaa_forecasts.csv')
 round_wins_all = all_forecast_data[['rd1_win', 'rd2_win', 'rd3_win', 'rd4_win', 'rd5_win', 'rd6_win', 'rd7_win']]
-round_wins = round_wins_all[69: 136]
-team_names = all_forecast_data['team_name']
+round_wins = round_wins_all[68: 136]
+team_names = all_forecast_data['team_name'][68:136]
+round_wins_named = round_wins.set_index(team_names)
+
 
 # create empty bracket
-bracket = np.zeros(63)
-
+bracket = list(np.zeros(63))
+full = False
 # ******** Algorithm ****************
 
 # loop that runs 10,000 iterations
 
 # loop that iterates through the empty games
+while not full:
+    game = bracket.index(0) + 1 # because of zero indexing
 
-# choose winner of championship
-winner = np.random.choice(team_names, 1, p=round_wins[:, 7])
+    # makes list of all teams playing in game
+    possible_teams = [team for team in path_data.map_dict if game in path_data.map_dict[team]]
 
-games = path_data.map_dict[winner]
+    # Find the round
+    one_team = possible_teams[0]
+    that_map = path_data.map_dict[one_team]
+    round = that_map.index(game) + 2
 
-for game in games:
-    bracket[game] = winner
+    # the probabilites of each of the teams winning in this round
+    probabilities = list(round_wins_named['rd{}_win'.format(round)][possible_teams])
 
 
+    # choose winner of championship
+    winner = np.random.choice(possible_teams, 1, p=probabilities)[0]
+
+    games = path_data.map_dict[winner]
+
+    for game in games:
+        if bracket[game-1] == 0:
+            bracket[game-1] = winner
+
+    if 0 not in bracket:
+        full = True
+
+    print(bracket)
+
+print('done')
 
 
 
